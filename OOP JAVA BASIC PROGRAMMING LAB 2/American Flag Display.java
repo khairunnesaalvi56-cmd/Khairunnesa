71dcf5182480f40127ee5c5dc1c2b001470ae0c9
khairunnesaalvi56-cmd <khairunnesaalvi56@gmail.com>
2026-09-23 13:20:50 +0600

    Add American Flag Display program

---
+++ OOP JAVA BASIC PROGRAMMING LAB 2/American Flag Display.java
@@ -0,0 +1,19 @@
+public class AmericanFlag {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i++) {
+
+            if (i % 2 != 0) {
+                System.out.println("* * * * * * ================================");
+            } else {
+                System.out.println("* * * * * ==================================");
+            }
+        }
+
+        System.out.println("============================================");
+        System.out.println("============================================");
+        System.out.println("============================================");
+        System.out.println("============================================");
+        System.out.println("============================================");
+    }
+}
