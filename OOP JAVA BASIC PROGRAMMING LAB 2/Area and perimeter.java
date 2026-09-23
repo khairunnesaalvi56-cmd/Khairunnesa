8f6048a4b819320b1f39c2387e8191bb31c242e9
khairunnesaalvi56-cmd <khairunnesaalvi56@gmail.com>
2026-09-23 13:19:01 +0600

    Add Rectangle class to calculate area and perimeter

---
+++ OOP JAVA BASIC PROGRAMMING LAB 2/Area and perimeter.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        double width = 5.5;
+        double height = 8.5;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("Width = " + width);
+        System.out.println("Height = " + height);
+        System.out.println("Area is = " + area);
+        System.out.println("Perimeter is = " + perimeter);
+    }
+}
