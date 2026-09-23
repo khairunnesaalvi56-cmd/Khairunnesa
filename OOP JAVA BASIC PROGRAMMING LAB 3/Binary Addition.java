ce42fff14a1104a96336e1e46a1304b571dd78f7
khairunnesaalvi56-cmd <khairunnesaalvi56@gmail.com>
2026-09-23 13:38:03 +0600

    Create Binary Addition.java

---
+++ OOP JAVA BASIC PROGRAMMING LAB 3/Binary Addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input first binary number: ");
+        String b1 = sc.next();
+        System.out.print("Input second binary number: ");
+        String b2 = sc.next();
+
+        int n1 = Integer.parseInt(b1, 2);
+        int n2 = Integer.parseInt(b2, 2);
+        int sum = n1 + n2;
+        String result = Integer.toBinaryString(sum);
+
+        System.out.println("Sum of two binary numbers: " + result);
+    }
+}
